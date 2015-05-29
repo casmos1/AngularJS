2556edcab77d07b0a7e839c11dd8c96c82665156
Mason Banks <mason.banks@extendhealth.com>
2015-05-29 12:30:11 -0600

    add exercise 4

---
+++ Exercise-02-04/controller.js
@@ -0,0 +1,46 @@
+﻿angular.module('maintenance', [])
+  .controller('siteEditCtrl', SiteEditCtrl);
+
+function SiteEditCtrl($scope) {
+  $scope.sites = sites;
+  $scope.startAdd = startAdd;
+  $scope.cancel = cancel;
+  $scope.add = add;
+  $scope.startRemove = startRemove;
+  $scope.remove = remove;
+  $scope.getSelected = getSelected;
+
+  setView('list');
+
+  function setView(view) {
+    $scope.view = view;
+  }
+
+  function startAdd() {
+    $scope.siteBox = '';
+    setView('add');
+  }
+
+  function cancel() {
+    setView('list');
+  }
+
+  function add() {
+    $scope.sites.push($scope.siteBox);
+    setView('list');
+  }
+
+  function startRemove(index) {
+    selected = index;
+    setView('delete');
+  }
+
+  function remove() {
+    $scope.sites.splice(selected, 1);
+    setView('list');
+  }
+
+  function getSelected() {
+    return sites[selected];
+  }
+}
