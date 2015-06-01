923daa8ba244ffedc8fc017a6bdba6efa0135697
Mason Banks <mason.banks@extendhealth.com>
2015-06-01 09:04:52 -0600

    added a few new exercises.

---
+++ Exercise-03-03/maintenance.js
@@ -0,0 +1,18 @@
+﻿angular.module('maintenance', ['ngRoute'])
+  .controller('adminCtrl', AdminCtrl)
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/locations', {
+        templateUrl: 'views/locations.html'
+      })
+      .when('/diveSites', {
+        templateUrl: 'views/sites.html'
+      })
+      .otherwise({
+        templateUrl: 'views/main.html'
+    });
+  });
+
+function AdminCtrl($scope) {
+}
+
